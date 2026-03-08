d1daf176f00a69dc1c52ba44e0671640b2193a53
Vinut Maradur <vinutpmaradur@gmail.com>
2026-03-08 15:26:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        file = request.files["file_upload"]
+
+        if file.filename == "":
+            return render_template("index.html", pred="No file selected")
+
+        file_path = os.path.join("uploads", file.filename)
+        os.makedirs("uploads", exist_ok=True)
+        file.save(file_path)
+
+        return render_template("index.html", pred="File Uploaded Successfully")
+
+    return render_template("index.html", pred="")
 
 if __name__ == "__main__":
     app.run()
