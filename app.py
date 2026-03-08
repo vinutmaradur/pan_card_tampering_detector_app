4e5c30ac257b88f7b9ca9f860ce237fd499a5026
Vinut Maradur <vinutpmaradur@gmail.com>
2026-03-08 15:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from app import app
+from flask import Flask
 
-if __name__ == '__main__':
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
     app.run()
